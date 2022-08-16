b365ce10a504ed3cb68c6a6ba4c8184faf246990
vwital <wital128@tut.by>
2022-08-16 17:31:17 +0300

    feat: add greeting and name

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 //Время
 const time = document.querySelector(".time");
 const myDate = document.querySelector(".date");
+const greeting = document.querySelector(".greeting");
 function showTime() {
   const date = new Date();
   const currentTime = date.toLocaleTimeString();
   time.textContent = currentTime;
   showDate();
+  getTimeOfDay();
   setTimeout(showTime, 1000);
 }
 showTime();
@@ -22,3 +24,29 @@ function showDate() {
   const currentDate = date.toLocaleDateString("en-US", options);
   myDate.textContent = currentDate;
 }
+// Время суток
+
+function getTimeOfDay() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay = "";
+  if (hours >= 0 && hours < 6) timeOfDay = "night";
+  if (hours >= 6 && hours < 12) timeOfDay = "morning";
+  if (hours >= 12 && hours < 18) timeOfDay = "afternoon";
+  if (hours >= 18 && hours < 24) timeOfDay = "evening";
+  greeting.textContent = `Good ${timeOfDay}`;
+}
+//Имя
+
+const name = document.querySelector(".name");
+function setLocalStorage() {
+  localStorage.setItem("name", name.value);
+}
+window.addEventListener("beforeunload", setLocalStorage);
+
+function getLocalStorage() {
+  if (localStorage.getItem("name")) {
+    name.value = localStorage.getItem("name");
+  }
+}
+window.addEventListener("load", getLocalStorage);
